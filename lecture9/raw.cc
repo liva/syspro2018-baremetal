775870718c026dd0ff1ec2c38343864e3c6a9103
liva <sap.pcmail@gmail.com>
2018-06-23 00:48:35 +0900

    support VirtualBox environment

--- lecture9/raw.cc
+++ lecture9/raw.cc
@@ -7,6 +7,8 @@ void init();
 void return_value(int rval);
 void putchar(char c);
 
+int height = 100;
+
 int main() {
   init();
   char buf[] = "booted\n";
@@ -21,8 +23,12 @@ int main() {
 
   pml4t[0] = ((uint64_t)pdpt + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
   pdpt[0]  = ((uint64_t)pd1 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pdpt[3]  = ((uint64_t)pd1 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
   for(int i = 0; i < 512; i++) {
     pd1[i]  = (0x80000000UL + 0x200000UL * i) | (1 << 0) | (1 << 1) | (1 << 2) | (1<<7);
+  }
+  for(int i = 0; i < 512; i++) {
+    pd2[i]  = (0xC0000000UL + 0x200000UL * i) | (1 << 0) | (1 << 1) | (1 << 2) | (1<<7);
   }
   
   asm volatile("movq %0, %%cr3"::"r"((uint64_t)pml4t + 0x80000000UL));
@@ -31,6 +37,21 @@ int main() {
   //!!!!!!!!!!!!!!!!!!!!!!
   // add your codes here!
   //!!!!!!!!!!!!!!!!!!!!!!
+  // for (int y = 0; y < height; y++) {
+  //   for (int x = 0; x < hw_info->framebuffer_width; x++) {
+  //     hw_info->framebuffer_addr[(y * hw_info->framebuffer_width + x) * 4] = 0xff;
+  //   }
+  // }
+  // for (int y = height; y < height + 10; y++) {
+  //   for (int x = 0; x < hw_info->framebuffer_width; x++) {
+  //     hw_info->framebuffer_addr[(y * hw_info->framebuffer_width + x) * 4 + 2] = 0xff;
+  //   }
+  // }
+  for (int y = 0; y < 10; y++) {
+    for (int x = 0; x < hw_info->framebuffer_width; x++) {
+      hw_info->framebuffer_addr[(y * hw_info->framebuffer_width + x) * 4 + 2] = 0xff;
+    }
+  }
 
   return_value(0);
   
